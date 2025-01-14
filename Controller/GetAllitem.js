8c4775592bdd85ef2e47179ee2e1969cc233bdc0
Rabbi Hossain <rabbihossainlhp@gmail.com>
2025-01-14 17:00:24 +0600

    added full crud

---
+++ Controller/GetAllitem.js
@@ -0,0 +1,18 @@
+//dependencies.....
+const db = require("../Db");
+
+//main function to get single item from table...
+const getAllItem = (req,res)=>{
+    const Query = `Select * from District_information3`;
+
+    db.query(Query,(error, result)=>{
+        if(error){
+            res.status(404).json({erMsg:"any information not found"});
+        }else{
+            res.status(200).json({SuccessMsg:"Sucessfully founded the information",result:result});
+        }
+    });
+};
+
+//Exporting the module...
+module.exports = getAllItem;; 
--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -3,13 +3,15 @@ const Router = require("express").Router();
 const createItem = require("../Controller/CreateItem");
 const getSingleItem = require("../Controller/GetSingleItem");
 const updateItem = require("../Controller/UpdateItem");
+const getAllItema = require("../Controller/GetAllitem");
+const deleteItem = require("../Controller/DeleteItem");
 
 
-Router.get("/allitems",);
+Router.get("/allitems",getAllItema);
 Router.get("/singleitem/:id",getSingleItem);
 Router.post("/createitem",createItem);
 Router.post("/updateitem/:id",updateItem);
-Router.delete("/deleteitem/:id",);
+Router.delete("/deleteitem/:id",deleteItem);
 
 
 
